b1cba3ec0f7d19f12e31ddc806aa18b98e5cdc5e
dkur <dkur@ciklum.com>
2022-09-11 16:25:44 +0200

    Remove deprecated WebSecurityConfigurerAdapter

--- src/main/kotlin/com/lucky/scoreboard/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/lucky/scoreboard/security/WebSecurityConfig.kt
@@ -6,21 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig {
     @Autowired
     lateinit var userDetailsService: UserDetailsServiceImpl
 
@@ -32,25 +33,26 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
         return AuthTokenFilter()
     }
 
-    @Throws(Exception::class)
-    public override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder) {
-        authenticationManagerBuilder.userDetailsService<UserDetailsService?>(userDetailsService)
-            .passwordEncoder(passwordEncoder())
-    }
-
     @Bean
-    @Throws(Exception::class)
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
+    @Throws(java.lang.Exception::class)
+    fun authenticationManager(authConfiguration: AuthenticationConfiguration): AuthenticationManager? {
+        return authConfiguration.authenticationManager
+    }
+    @Bean
+    fun authenticationProvider(): DaoAuthenticationProvider {
+        val authProvider = DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService)
+        authProvider.setPasswordEncoder(passwordEncoder())
+        return authProvider
     }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
-
+    @Bean
     @Throws(Exception::class)
-    override fun configure(http: HttpSecurity) {
+    fun configure(http: HttpSecurity): SecurityFilterChain {
         http.cors().and().csrf().disable()
             .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -58,5 +60,7 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
             .antMatchers("/api/scores/**").permitAll()
             .anyRequest().authenticated()
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
+        http.authenticationProvider(authenticationProvider());
+        return http.build()
     }
 }
